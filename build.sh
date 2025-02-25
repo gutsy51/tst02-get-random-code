79051940bbba6e0b8da287ba6982401ccf08395d
Poluianov Valeriy <bestcom5055@gmail.com>
2025-02-25 15:49:12 +0300

    Fix: Add static into the build

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -o errexit  # Exit on error
 
 pip install -r requirements.txt
 
-#echo "Collecting static files..."
-#python RandomCode/manage.py collectstatic --noinput
-#ls -l RandomCode/static/
+echo "Collecting static files..."
+python RandomCode/manage.py collectstatic --noinput
+ls -l RandomCode/static/
 
 echo "Migrating database..."
 python RandomCode/manage.py migrate
